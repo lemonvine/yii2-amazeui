ded70cbfb3f8697f192c4900fc7f0dafe16623ad
sdd8002@163.com <sdd8002@163.com>
2018-08-12 14:45:37 +0800

    test

---
+++ src/Lemon/Amazeui/Rose.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lingan
+ * Date: 2017/11/6
+ * Time: 21:39
+ */
+
+namespace lemon\amazeui;
+
+
+class Rose
+{
+    public function desc()
+    {
+        echo "this is rose flower";
+    }
+}
