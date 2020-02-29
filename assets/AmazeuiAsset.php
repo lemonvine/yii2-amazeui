48080e032e22d11221daf3eceeed8a10c60ae92d
lemonvine <sdd8002@163.com>
2020-02-29 19:20:03 +0800

    完善

---
+++ assets/AmazeuiAsset.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace mango\assets;
+
+use yii\web\AssetBundle;
+
+
+class AmazeuiAsset extends AssetBundle
+{
+	public static $path = '@vendor/npm-asset/amazeui/dist';
+		
+	public function init()
+	{
+		$this->sourcePath = self::$path;
+		$postfix = YII_DEBUG ? '' : '.min';
+		$version = Yii::$app->params['admin_version'];
+		
+		$this->js[] = "js/amazeui{$postfix}.js?v=v{$version}";
+		
+		$this->css[] = "css/amazeui{$postfix}.css?v=v{$version}";
+		
+		parent::init();
+	}
+}
